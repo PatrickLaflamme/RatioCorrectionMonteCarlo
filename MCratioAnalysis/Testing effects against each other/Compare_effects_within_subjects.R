6298fcb4fd0d0d47e2b2353e24c088d95700ff35
PatrickLaflamme <Patrickj.laflamme@gmail.com>
2017-10-26 13:33:43 -0700

    Undo previous changes

--- MCratioAnalysis/Testing effects against each other/Compare_effects_within_subjects.R
+++ MCratioAnalysis/Testing effects against each other/Compare_effects_within_subjects.R
@@ -27,7 +27,6 @@ if(length(args) > 0){
         .Call("mpi_finalize") 
       } 
     }
-    mpi.spawn.Rslaves()
   } 
   
   if( length(path) ) {
--- MCratioAnalysis/Testing effects against each other/Generate_within_subject_data_files.R
+++ MCratioAnalysis/Testing effects against each other/Generate_within_subject_data_files.R
@@ -26,8 +26,6 @@ if(length(args) > 0){
         .Call("mpi_finalize") 
       } 
     }
-    
-    mpi.spawn.Rslaves()
   } 
   
   if( length(path) ) {
